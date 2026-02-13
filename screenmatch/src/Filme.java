e4c2fa93efc29ef3b055a8d479b9eb741c4fe3be
pcandradedev <pcandradedev@gmail.com>
2026-02-13 13:39:02 -0300

    Criando Objeto Filme é atribuindo valores

---
+++ screenmatch/src/Filme.java
@@ -0,0 +1,9 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    int duracaoEmMinutos;
+
+    public void exibir() {
+        System.out.println("Nome: " + nome + " ano de lançamento: " + anoDeLancamento + "duração do filme: " + duracaoEmMinutos);
+    }
+}
--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,2 +1,15 @@
 public class Principal {
+    public static void main(String[] args) {
+
+        Filme primeiroFilme = new Filme();
+        primeiroFilme.nome = "Top Gun Maverick";
+        primeiroFilme.anoDeLancamento = 1970;
+        primeiroFilme.duracaoEmMinutos = 180;
+
+
+
+
+
+
+    }
 }
